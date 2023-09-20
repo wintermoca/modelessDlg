df0cd650c72c20334b176d139a2a7e8244b7f299
권윤호 <yoonhoktx@gmail.com>
2023-09-21 01:42:51 +0900

    초기 커밋.

---
+++ FC/AddClass.cpp
@@ -0,0 +1,46 @@
+﻿// AddClass.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "FC.h"
+#include "afxdialogex.h"
+#include "AddClass.h"
+
+
+// AddClass 대화 상자
+
+IMPLEMENT_DYNAMIC(AddClass, CDialogEx)
+
+AddClass::AddClass(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_SON, pParent)
+	, m_Str(_T(""))
+{
+
+}
+
+AddClass::~AddClass()
+{
+}
+
+void AddClass::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_Str);
+}
+
+
+BEGIN_MESSAGE_MAP(AddClass, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &AddClass::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// AddClass 메시지 처리기
+
+#include "FCDlg.h"//(프로젝트 이름) + Dlg.h
+void AddClass::OnBnClickedButton1()
+{
+	CFCDlg* p = (CFCDlg*)GetParent(); //C + (프로젝트 이름) + Dlg  
+	UpdateData(true);
+	p->m_List.AddString(m_Str);
+	//Add 버튼에 기능을 부여하는 코드
+}
---
+++ FC/AddClass.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// AddClass 대화 상자
+
+class AddClass : public CDialogEx
+{
+	DECLARE_DYNAMIC(AddClass)
+
+public:
+	AddClass(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~AddClass();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SON };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	CString m_Str;
+};
